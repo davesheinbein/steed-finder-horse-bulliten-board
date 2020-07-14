b33ee6a6221a483e460c1a818f9907c38a25de20
David Sheinbein <davesheinbein@gmail.com>
2020-07-14 15:58:58 -0700

    update to title text

--- src/components/BarnImg/BarnImg.jsx
+++ src/components/BarnImg/BarnImg.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import barn from './barn.png'; 
+import './BarnImg.css'
 
 // console.log(barn); 
 
 function Barn() {
   // Import result is the URL of your image
-  return <img src={barn} alt="Barn" />;
+  return(
+    <div>
+      <img src={barn} alt="Barn" className='barnImg'/>
+      <div className='titleText'>Steed Finder</div>
+    </div>
+  ) 
 }
 export default Barn;
