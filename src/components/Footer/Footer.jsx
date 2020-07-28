4b49edc5d38a105d44c202e3b28e994a6be9e3d8
David Sheinbein <davesheinbein@gmail.com>
2020-07-28 14:50:40 -0700

    update to footer css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import barn from '../BarnImg/barn.png';
 import './Footer.css'
-import FooterTop from '../FooterTop/FooterTop'
+import horse from './horse_silhouette.png';
 
 
 function Footer() {
   // Import result is the URL of your image
   return (
     <>
-      <div className='displayBlock'>
-        <FooterTop />
-        <div className='footer-container'>
-          <div className='footer-left'>Created by D. Sheinbein <img src={barn} alt="Barn" className='barnFooterImg'></img></div>
-          <div className='footer-right'>All Rights Reserved, &copy; 2020 Steed Finder</div>
+      <div className='page-footer'>
+        <div className="container">
+          <div className="row">
+            <div className="col l6 s12">
+              <h5 className="white-text footer-header">Additional Resources</h5>
+              <img src={horse} alt="Horse" className='horseImg' />
+            </div>
+            <div className="col l4 s12">
+              <h5 className="white-text footer-header">Links</h5>
+              <ul>
+                <li className='footer-title-link'><a href='/'>Home</a></li>
+                <li>Adopt an Animal: <a href='https://www.petfinder.com/'>https://www.petfinder.com/</a></li>
+                <li>Racing Stats: <a href='https://www.equibase.com/stats/'>https://www.equibase.com/stats/</a></li>
+                <li>Report Animal Abuse: <a href='https://www.humanesociety.org/resources/report-animal-cruelty'>https://www.humanesociety.org/</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
+        <div className="footer-copyright">
+          <div className="container bottom-container">
+            <div className='left left-text'>Created by D. Sheinbein <img src={barn} alt="Barn" className='barnFooterImg'></img></div>
+            <div className='right right-text'>All Rights Reserved, &copy; 2020 Steed Finder</div>
+          </div>
+        </div>
+
       </div>
     </>
   )
--- src/components/FooterTop/FooterTop.jsx
+++ src/components/FooterTop/FooterTop.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import horse from './horse_silhouette.png';
-import './FooterTop.css'
+// import React from 'react';
+// import horse from './horse_silhouette.png';
+// import './FooterTop.css'
 
 
-function FooterTop() {
-    // Import result is the URL of your image
-    return (
-        <>
-            <div className='footer-top-container'>
-                    <div className='footer-top-left'>
-                    <img src={horse} alt="Horse" className='horseImg' />
-                    </div>
-                    <div className='footer-top-right'>
-                        <div className='footer-title-link'><a href='/'>Home</a></div>
-                        <div className='footer-title'>Additional Resources</div>
-                        <ul>
-                            <li>Adopt an Animal: <a href='https://www.petfinder.com/'>https://www.petfinder.com/</a></li>
-                            <li>Racing Stats: <a href='https://www.equibase.com/stats/'>https://www.equibase.com/stats/</a></li>
-                            <li>Report Animal Abuse: <a href='https://www.humanesociety.org/resources/report-animal-cruelty'>https://www.humanesociety.org/resources/report-animal-cruelty</a></li>
-                        </ul>
-                    </div>
-            </div>
-        </>
-    )
-}
-export default FooterTop;
+// function FooterTop() {
+//     // Import result is the URL of your image
+//     return (
+//         <>
+//             <div className='footer-top-container'>
+//                     <div className='footer-top-left'>
+//                     <img src={horse} alt="Horse" className='horseImg' />
+//                     </div>
+//                     <div className='footer-top-right'>
+//                         <div className='footer-title-link'><a href='/'>Home</a></div>
+//                         <div className='footer-title'>Additional Resources</div>
+//                         <ul>
+//                             <li>Adopt an Animal: <a href='https://www.petfinder.com/'>https://www.petfinder.com/</a></li>
+//                             <li>Racing Stats: <a href='https://www.equibase.com/stats/'>https://www.equibase.com/stats/</a></li>
+//                             <li>Report Animal Abuse: <a href='https://www.humanesociety.org/resources/report-animal-cruelty'>https://www.humanesociety.org/resources/report-animal-cruelty</a></li>
+//                         </ul>
+//                     </div>
+//             </div>
+//         </>
+//     )
+// }
+// export default FooterTop;
